062f2fa9b947293f7b8a26ef2389ac104d971a44
Jonathan Ingall <jonny@mondago.com>
2020-10-08 14:10:26 +0100

    Only build image we need

--- build.sh
+++ build.sh
@@ -10,13 +10,9 @@ buildnumber=${4-$(date -u +"%y%m%d%H%M")}
 docker login -u "$2" -p "$3"
 
 # build base images
-docker build -q -t "$1"/php:7.0-apache_"$buildnumber" 7.0-apache
-docker build -q -t "$1"/php:7.2-apache_"$buildnumber" 7.2-apache
 docker build -q -t "$1"/php:7.3-apache_"$buildnumber" -t "$1"/php:latest_"$buildnumber" 7.3-apache
 docker tag "$1"/php:latest_"$buildnumber" "$1"/php:latest
 
-docker push "$1"/php:7.0-apache_"$buildnumber"
-docker push "$1"/php:7.2-apache_"$buildnumber"
 docker push "$1"/php:7.3-apache_"$buildnumber"
 docker push "$1"/php:latest_"$buildnumber"
 docker push "$1"/php:latest
